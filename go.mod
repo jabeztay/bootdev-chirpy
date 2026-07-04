2ac2c42fdbd671027e4e983833b049ebe565606a
jabeztay <jabeztay@gmail.com>
2026-07-04 10:11:25 +0800

    basic go server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jabeztay/bootdev-chirpy
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+	s := &http.Server{Addr: ":" + port, Handler: mux}
+
+	log.Printf("Serving on port %s\n", port)
+	log.Fatal(s.ListenAndServe())
+}
